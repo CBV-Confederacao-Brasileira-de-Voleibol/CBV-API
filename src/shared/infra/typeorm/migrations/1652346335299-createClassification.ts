5a439088617ad3cee27fdeb854933b761763b126
Msouza <msdsantos96@gmail.com>
2022-05-12 06:07:24 -0300

    create migation

---
+++ src/shared/infra/typeorm/migrations/1652346335299-createClassification.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createClassification1652346335299 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
