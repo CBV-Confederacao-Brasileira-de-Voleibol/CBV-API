83a537fc2295dfe28de15b3e9249cfc1f5a52940
Msouza <msdsantos96@gmail.com>
2022-05-12 06:08:05 -0300

    project config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,5 @@
-import app from './app';
-import '../typeorm'
+import app from "./app";
 
-
-app.listen(process.env.APP_PORT, () =>{
-    console.log(`🚀 Server is running on port ${process.env.APP_PORT}`)
-})
+app.listen(process.env.APP_PORT, () => {
+    console.log(`🚀 Server is running on port ${process.env.APP_PORT}`);
+});
