2db4f0fc35422af0e34a2aa00340f1d094f932dc
Msouza <msdsantos96@gmail.com>
2022-05-19 11:12:28 -0300

    created CompetitionController

---
+++ src/modules/statistics/infra/http/controllers/CompetitionContoller.ts
@@ -0,0 +1,27 @@
+import { classToClass } from 'class-transformer';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateCompetitionService from '@modules/statistics/services/CreateCompetitionService';
+
+class CompetitionContoller {
+    async create(request: Request, response: Response): Promise<Response> {
+        const { name, year, date_start, date_end, type } = request.body;
+
+        const createCompetitionService = container.resolve(
+            CreateCompetitionService,
+        );
+
+        const competition = await createCompetitionService.execute({
+            name,
+            year,
+            date_start,
+            date_end,
+            type,
+        });
+
+        return response.json({ competition: classToClass(competition) });
+    }
+}
+
+export default CompetitionContoller;
