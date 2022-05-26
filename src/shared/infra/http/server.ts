a685325a236318dc2b281a17b2fabd553244314c
Msouza <msdsantos96@gmail.com>
2022-05-26 01:49:49 -0300

    add

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
 app.listen(process.env.PORT, () => {
-    console.log(`🚀 Server is running on port ${process.env.DATABASE_URL}`);
+    console.log(`🚀 Server is running on port ${process.env.PORT}`);
 });
