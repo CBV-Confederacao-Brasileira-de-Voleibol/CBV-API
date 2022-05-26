08c4d823e2eaef970c102093f34a2da2736c5998
Msouza <msdsantos96@gmail.com>
2022-05-26 02:07:14 -0300

    add

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,11 @@
 module.exports = {
-    url: process.env.DATABASE_URL,
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+
     entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
     migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
   
